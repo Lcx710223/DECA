aa260a636a65abbceeebe11fba9fce4f08b5d30d
LCX@ARGOS <911239555@qq.com>
2025-08-23 14:40:07 +0800

    Update deca.py

--- decalib/deca.py
+++ decalib/deca.py
@@ -74,19 +74,10 @@ class DECA(nn.Module):
         model_path = self.cfg.pretrained_modelpath
         if os.path.exists(model_path):
             print(f'trained model found. load {model_path}')
-
-            # 检测GPU/CPU兼容。20250819COPILOT.
-            if torch.cuda.is_available():
-              device = torch.device('cuda')
-              print("GPU detected. Loading model to CUDA.")
-              checkpoint = torch.load(model_path)
-
-            else:
-              device = torch.device('cpu')
-              print("No GPU detected. Loading model to CPU.")
-              checkpoint = torch.load(model_path, map_location=device)
+            ###JULES20250832检测CPU/GPU兼容性。
+            print(f"Loading model to {self.device}")
+            checkpoint = torch.load(model_path, map_location=self.device)
             
-            #checkpoint = torch.load(model_path)
             self.checkpoint = checkpoint
             util.copy_state_dict(self.E_flame.state_dict(), checkpoint['E_flame'])
             util.copy_state_dict(self.E_detail.state_dict(), checkpoint['E_detail'])
