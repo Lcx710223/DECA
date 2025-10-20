2cecd2c06dde0b5410d2fc5acb4b213bde9302ef
LCX@ARGOS <911239555@qq.com>
2025-10-20 21:44:57 +0800

    Update detectors.py

--- decalib/datasets/detectors.py
+++ decalib/datasets/detectors.py
@@ -22,7 +22,7 @@ class FAN(object):
         ### LCX251020修改兼容性：CPU/GPU
         if device is None:
             device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        self.model = face_alignment.FaceAlignment(face_alignment.LandmarksType.TOW_D, flip_input=False, device=device)
+        self.model = face_alignment.FaceAlignment(face_alignment.LandmarksType.TWO_D, flip_input=False, device=device)
 
     def run(self, image):
         '''
